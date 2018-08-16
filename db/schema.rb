1969a09839187306242de8daaf12143cd5c7d790
Dani Lyn <dlyn1110@gmail.com>
2018-08-16 20:36:25 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,5 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_03_14_210852) do
-  create_table "users", force: :cascade do |t|
-  t.string "username"
-  t.string "password_digest"
-end
 
 end
